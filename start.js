e7481a9196255379939e69e04365e75d2b9dedee
ingaku <ingaku@slack-corp.com>
2021-07-09 12:59:24 -0400

    Edited async callback functions implementation

--- start.js
+++ start.js
@@ -3,22 +3,30 @@ require("dotenv").config();
 const fetch = require('node-fetch');
 
 // GET data asynchronously with callback
-function loadCountryDataWithCallback(callbackFunc) {
+function loadCountryDataWithCallback(callback) {
     var requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
     let response = fetch(`http://api.airvisual.com/v2/countries?key=${process.env.API_KEY}`, requestOptions);
-    callbackFunc(response);      
+ 
+    callback(response, function(data){
+        console.log(data);
+    }, function(e) {
+        console.log(e);
+    });      
 }
 
-function logData(data) {
-    data.then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+function callbackFunc(response, callback1, failureCallback) {
+    if (!response.ok) {
+        failureCallback(response.error);
+    }
+    else {
+        callback1(response.text());
+    }
 }
 
-loadCountryDataWithCallback(logData);
+loadCountryDataWithCallback(callbackFunc);
 
 // GET data asynchronously, no callback
 function loadCountryData() {
